cf5fd93f98aa1e76476cb4020bfaf46762e88dac
eldurato99-ctrl <eldurato99@gmail.com>
2026-06-16 16:28:15 +0530

    Update dashboard.php

--- pages/account/dashboard.php
+++ pages/account/dashboard.php
@@ -1,6 +1,8 @@
 <?php
 // pages\account\dashboard.php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if(!isset($_SESSION['user_id'])) {
     header("Location: ../auth/login.php");
     exit;
