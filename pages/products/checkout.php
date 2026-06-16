b8155cd88f560b25c861f7bff45d02c3e64d9dd0
eldurato99-ctrl <eldurato99@gmail.com>
2026-06-16 16:26:33 +0530

    Update checkout.php

--- pages/products/checkout.php
+++ pages/products/checkout.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once '../../config/database.php';
 
 // GET se targeted product key check karein jo user ne select ki hai
