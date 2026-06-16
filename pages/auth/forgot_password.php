da47e03efb068df3ce0a8c66f80572f692b21191
eldurato99-ctrl <eldurato99@gmail.com>
2026-06-16 16:56:01 +0530

    Update forgot_password.php

--- pages/auth/forgot_password.php
+++ pages/auth/forgot_password.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 require_once '../../config/database.php';
-require_one  '../../config/config.php';
+require_once  '../../config/config.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require_once __DIR__ . '/../../vendor/autoload.php';
