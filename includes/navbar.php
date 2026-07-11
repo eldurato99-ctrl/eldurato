7d023239636b390a782f99157ef1d9bee6ccfad0
eldurato99-ctrl <eldurato99@gmail.com>
2026-07-11 14:25:53 +0530

    fixed edit button

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,4 @@
 <?php 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 require_once __DIR__ . '/../config/functions.php';
 require_once __DIR__ . '/../config/database.php'; 
 ?>
